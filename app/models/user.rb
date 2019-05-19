3989fb81ac754c3673985da95196636354105138
xuantien <xuantien1026@gmail.com>
2019-05-19 23:13:53 +0700

    Add very simple email validation with regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :microposts
     validates :name, presence: true
-    validates :email, presence: true
+    validates :email, presence: true, format: { with: /[a-zA-Z0-9_]+@[a-z]+\.[a-z]+/}
 end
