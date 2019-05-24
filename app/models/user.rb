401ad0542943bd00821c0bdee2d074483d1baf83
xuantien <xuantien1026@gmail.com>
2019-05-24 14:37:02 +0700

    Change valid email REGEX and write test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :microposts
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :name,  presence: true, length: {maximum: 50}
-    validates :email, presence: true, length: {maximum: 255}, format: { with: /[a-zA-Z0-9_]+@[a-z]+\.[a-z]+/, message: "must be of the form someone@something.com"}
+    validates :email, presence: true, length: {maximum: 255}, format: { with: VALID_EMAIL_REGEX, message: "must be of the form someone@something.com"}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,4 +28,16 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid?
   end
+
+  test "email validation should except valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.com
+                        A_US-ER@foo.bar.org
+                        first.last@foo.jp
+                        alice+bob@bar.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+  
 end
