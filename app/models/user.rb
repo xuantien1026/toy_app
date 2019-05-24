322b1a784486c64ba8633a997aede0dc26fd3aa0
xuantien <xuantien1026@gmail.com>
2019-05-24 14:12:07 +0700

    Add lenghth validation for user name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :microposts
-    validates :name, presence: true
-    validates :email, presence: true, format: { with: /[a-zA-Z0-9_]+@[a-z]+\.[a-z]+/, message: "must be of the form someone@something.com"}
+    validates :name,  presence: true, length: {maximum: 50}
+    validates :email, presence: true, length: {maximum: 255}, format: { with: /[a-zA-Z0-9_]+@[a-z]+\.[a-z]+/, message: "must be of the form someone@something.com"}
 end
