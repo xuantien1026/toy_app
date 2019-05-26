780d5ed557d4d0a2331e2c024ed39c9e6dad3bf0
xuantien <xuantien1026@gmail.com>
2019-05-26 22:58:33 +0700

    Add search form & search logic, fix some styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,11 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    if params[:search]
+      @users = User.where('email like ?', "%#{params[:search]}%")
+    else
+      @users = User.all
+    end
   end
 
   # GET /users/1
@@ -70,6 +74,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password)
+      params.require(:user).permit(:name, :email, :password, :search)
     end
 end
